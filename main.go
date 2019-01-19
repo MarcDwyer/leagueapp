4b8d11b0c27822ea8e2769aeadeb9016342d5a2a
Marc Dwyer <marcdwyer@marcs-imac.attlocal.net>
2019-01-19 14:01:46 -0600

    added handling of invalid summoner searches

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -25,36 +24,6 @@ func init() {
 
 // /lol/summoner/v4/summoners/by-name/{summonerName}
 func main() {
-	http.HandleFunc("/api/stats", stats)
+	http.HandleFunc("/api/stats/", Stats)
 	log.Fatal(http.ListenAndServe(":5000", nil))
-}
-
-func stats(w http.ResponseWriter, r *http.Request) {
-	ch := make(chan map[string]interface{})
-	go func() {
-		defer func() {
-			close(ch)
-		}()
-		url := fmt.Sprintf("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Santorin?api_key=%v", key)
-		res, err := http.Get(url)
-		if err != nil {
-			fmt.Println(err)
-		}
-		var data map[string]interface{}
-
-		err = json.NewDecoder(res.Body).Decode(&data)
-		if err != nil {
-			fmt.Println(err)
-		}
-		if _, ok := data["id"]; !ok {
-			return
-		}
-		ch <- data
-	}()
-
-	go func() {
-		for v := range ch {
-			fmt.Println(v)
-		}
-	}()
 }
