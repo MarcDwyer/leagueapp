fbe78d2427a872f1593b455f9fd871b7fe05e04b
Marc Dwyer <marcdwyer1738@gmail.com>
2019-01-22 13:52:27 -0600

    passing channels as pointers is bad

--- api.go
+++ api.go
@@ -12,8 +12,8 @@ type Payload struct {
 	Match    chan map[string]interface{} `json:"match, omitempty"`
 }
 type Channel struct {
-	Summoner chan map[string]interface{}
-	Match    chan map[string]interface{}
+	Summoner map[string]interface{}
+	Match    map[string]interface{}
 }
 
 func Stats(w http.ResponseWriter, r *http.Request) {
@@ -36,23 +36,33 @@ func Stats(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	var ch Payload
-	defer func() {
-		close(ch.Summoner)
-		close(ch.Match)
+	// var super Channel
+
+	go func() {
+		for {
+			select {
+			case client, more := <-ch.Summoner:
+				fmt.Println("test")
+				fmt.Println(more)
+				fmt.Println(client)
+			}
+		}
 	}()
+	fmt.Println(ch)
 	str := fmt.Sprintf("/lol/league/v4/positions/by-summoner/%v?api_key=%v", data["id"], key)
 	go GetStats("summonerInfo", str, data, &ch)
+
 	str = fmt.Sprintf("/lol/match/v4/matchlists/by-account/%v?api_key=%v", data["accountId"], key)
 	go GetStats("matches", str, data, &ch)
-	payload, _ := json.Marshal(ch)
-	w.Write(payload)
+	fmt.Println(ch)
+	fmt.Println(<-ch.Match)
 }
 
 ///lol/match/v4/matchlists/by-account/{encryptedAccountId}
 
 func GetStats(req string, url string, data map[string]interface{}, pointer *Payload) {
 	fmt.Println("is this running?")
-
+	fmt.Println(pointer)
 	beg := fmt.Sprintf("https://na1.api.riotgames.com%v", url)
 	switch req {
 	case "summonerInfo":
@@ -64,20 +74,25 @@ func GetStats(req string, url string, data map[string]interface{}, pointer *Payl
 		var usr []map[string]interface{}
 		err = json.NewDecoder(rz.Body).Decode(&usr)
 		if err != nil {
+			fmt.Println("1")
 			fmt.Println(err)
 			return
 		}
 		pointer.Summoner <- usr[0]
+		fmt.Println(<-pointer.Summoner)
 	case "matches":
 		rz, err := http.Get(beg)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		err = json.NewDecoder(rz.Body).Decode(&pointer.Match)
+		var data map[string]interface{}
+		err = json.NewDecoder(rz.Body).Decode(&data)
 		if err != nil {
+			fmt.Println("2")
 			fmt.Println(err)
 			return
 		}
+		pointer.Match <- data
 	}
 }
